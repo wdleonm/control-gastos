2a088f6b54da3c2a8a4ed2b9bd33f2e18719d39f
Williams Leon <wdleonm@gmail.com>
2023-01-27 10:18:03 -0400

    100 Primeros Pasos con el Componente de Control de Presupuesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import NuevoPresupuesto from './NuevoPresupuesto'
+import ControlPresupuesto from './ControlPresupuesto'
 
 const Header = ({ 
     presupuesto, 
@@ -12,7 +13,9 @@ const Header = ({
         <h1>Planificador de Gastos</h1>
 
         {isValidPresupuesto ? (
-            <p>Control Presupuesto</p>
+            <ControlPresupuesto
+                presupuesto= {presupuesto}
+            />
         ) : (
             <NuevoPresupuesto 
                 presupuesto= {presupuesto}
